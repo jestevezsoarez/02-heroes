261476dcb5fe929a54ecaecff93d37c956181e87
Javier Estevez <javierdaniloestevez@gmail.com>
2021-02-02 17:56:10 -0300

    creacion del servicio y carga de los datos de los heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { APP_ROUTING } from './app.routes';
 
 // Servicios
+import { HeroesService } from './services/heroes.service';
 
 // Componentes
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +27,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
